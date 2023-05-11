c79f5658a3cef351390f2a8dc95413a8bc98e451
Damaris Espinosa <2130081@upv.edu.mx>
2023-05-11 16:56:10 -0600

    Intento 2

--- Main.java
+++ Main.java
@@ -3,9 +3,9 @@ class Main {
     public static void main(String[] args) {
     	int a = Integer.parseInt(args[0]);
     	int b = Integer.parseInt(args[1]);
-    	
+    	// crear un nuevo objeto 
 	SumaDosNumeros obj = new SumaDosNumeros();
-	
+	// imprimir la suma de los dos numeros enviados 
 	System.out.println(obj.suma(a,b));
     }
 }
--- SumaDosNumeros.java
+++ SumaDosNumeros.java
@@ -1,5 +1,6 @@
 class SumaDosNumeros {
     int suma(int a, int b) {
+    	// regresa el valor de la suma de los numeros enviados 
     	return a + b;
     }
 }
